c8167ccedbc9c5f0df74876a9bbad26e98721597
user <nohyoonha@gmail.com>
2023-04-29 22:06:21 +0900

    4/26 수업 완료

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -2,11 +2,9 @@ package com.example.demo.service;
 
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
-import com.sun.tools.javac.comp.Todo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.w3c.dom.ls.LSInput;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,66 +12,79 @@ import java.util.Optional;
 @Slf4j
 @Service
 public class TodoService {
-    @Autowired
-    public TodoRepository repository;
 
-    public String testService(){
+    @Autowired
+    private TodoRepository repository;
+
+    public String testService() {
         TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+
         repository.save(entity);
+
         TodoEntity savedEntity = repository.findById(entity.getId()).get();
 
         return savedEntity.getTitle();
     }
 
-    public List<TodoEntity> create(final TodoEntity entity){
-        validate(entity);
-
-        repository.save(entity);
-
-        log.info("Entity Id : {} is saved", entity.getId());
-
-        return repository.findByUserId(entity.getUserId());
-
-    }
-
-    private void validate(final TodoEntity entity){
-        if(entity == null){
+    public List<TodoEntity> create(final TodoEntity entity) {
+        if(entity == null) {
             log.warn("Entity cannot be null.");
             throw new RuntimeException("Entity cannot be null.");
         }
+
         if(entity.getUserId() == null) {
             log.warn("Unknown user.");
             throw new RuntimeException("Unknown user.");
         }
+
+        repository.save(entity);
+
+        log.info("Entity id: {} is saved.", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
+
+
     }
 
     public List<TodoEntity> retrieve(final String userId) {
         return repository.findByUserId(userId);
     }
 
-    public List<TodoEntity> update(final TodoEntity entity){
+    public List<TodoEntity> update(TodoEntity entity) {
         validate(entity);
+
         final Optional<TodoEntity> original = repository.findById(entity.getId());
-        original.ifPresent(todo ->{
+
+        original.ifPresent(todo -> {
             todo.setTitle(entity.getTitle());
             todo.setDone(entity.isDone());
 
             repository.save(todo);
         });
-        return  retrieve(entity.getUserId());
-    }
 
-    public List<TodoEntity> delete(final TodoEntity entity){
-        validate(entity);
-
-        try{
-            repository.delete(entity);
-        } catch (Exception e) {
-            log.error("error deleting entity", entity.getId(), e);
-
-            throw new RuntimeException("error deleting entity " + entity.getId());
-        }
         return retrieve(entity.getUserId());
     }
 
+    public List<TodoEntity> delete(final TodoEntity entity) {
+        validate(entity);
+
+        try {
+            repository.delete(entity);
+        } catch (Exception e) {
+            log.error("error deleting entity", entity.getId(), e);
+            throw new RuntimeException("error deleting entity" + entity.getId());
+        }
+
+        return retrieve(entity.getUserId());
+    }
+
+    private void validate(final TodoEntity entity) {
+        if(entity == null) {
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException(("Entity cannot be null."));
+        } if(entity.getUserId() == null) {
+            log.warn("Unknown user.");
+            throw new RuntimeException(("Unknown user."));
+        }
+    }
 }
